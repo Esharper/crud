9d1dde3f244f17eff4cb86a5ddbbf7e560361e47
esharper87 <Elginsharper87@gmail.com>
2024-06-21 16:08:45 -0500

    init

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <footer className="text-center">&copy; Elgin Sharper - Crud app </footer>
+      </body>
     </html>
   );
 }
