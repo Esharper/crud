a92b7d6fb781954ddb35a9d4c6ffd249eb9ef1c4
esharper87 <Elginsharper87@gmail.com>
2024-08-14 21:48:39 -0500

    init

--- src/app/components/books.jsx
+++ src/app/components/books.jsx
@@ -134,4 +134,25 @@ export default function BookComponent({
                 title="Delete this book"
                 className="p-2 rounded border-emerald-900 hover:bg-emerald-600"
               >
-      
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="text-red-700 w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 6l12 12m0-12L6 18"
+                  />
+                </svg>
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
